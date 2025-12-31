73a5b2f64a67bdeccaa2be954ff59aff90c55cea
jin214930 <jin214930@naver.com>
2025-12-31 09:18:10 +0900

    0037 - 각종 설정들을 application.yml 파일에 모아두기

--- src/main/java/com/ward/ddd/boundedContext/cash/domain/CashPolicy.java
+++ src/main/java/com/ward/ddd/boundedContext/cash/domain/CashPolicy.java
@@ -1,5 +1,14 @@
 package com.ward.ddd.boundedContext.cash.domain;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CashPolicy {
-    public static final long HOLDING_MEMBER_ID = 2L;
+    public static long HOLDING_MEMBER_ID;
+
+    @Value("${custom.global.holdingMemberId}")
+    public void setHoldingMemberId(long id) {
+        HOLDING_MEMBER_ID = id;
+    }
 }
